24957237f59cca32db2fba9d7fe2646644763d52
CrimsonCoder42 <nostro37@gmail.com>
2023-05-30 12:31:07 -0400

    06:36:00

--- my-react-app/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ my-react-app/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,24 @@
 import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+  }, []);
+
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return <h2>Multiple Returns Basics</h2>;
 };
 export default MultipleReturnsBasics;
--- my-react-app/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ my-react-app/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,56 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(url);
+        const user = await response.json();
+        setUser(user);
+        console.log(user)
+        setUser(user);
+      } catch (error) {
+        setIsError(true);
+        console.log(error);
+      }
+      setIsLoading(false);
+    }
+    fetchUser();
+  }, []);
+
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>There was an error</h1>
+      </div>
+    );
+  }
+
+  const { avatar_url, name, company, bio } = user;
+  return (
+    <div>
+      <img
+        style={{ width: '150px', borderRadius: '25px' }}
+        src={avatar_url}
+        alt={name}
+      />
+      <h2>{name}</h2>
+      <h4>works at {company}</h4>
+      <p>{bio}</p>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
